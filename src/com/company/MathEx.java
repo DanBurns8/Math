70aa973cead59aea17cf0fa9da7f8a18e3786856
Daniel Burns <danielburns@billericak12.com>
2021-09-20 11:54:14 -0400

    Daniel Burns

--- src/com/company/MathEx.java
+++ src/com/company/MathEx.java
@@ -1,5 +1,24 @@
 package com.company;
 
 public class MathEx {
-
+    public static void main(String[] args) {
+        System.out.println(swap(453));
+        timeleft(3,34, 8, 56);
+    }
+    public static int swap(int x) {
+        int ones = x%10;
+        int tens = x%100/10;
+        int hunds = x/100;
+                x = hunds*100 + ones*10 + tens;
+        return x;
+    }
+    public static void timeleft (int currHour, int currMin, int depHour, int depMin) {
+        int totalcurrtime = (currHour * 60 + currMin);
+        int totaldeptime = (depHour * 60 + depMin);
+        int differenceTime = (totaldeptime - totalcurrtime);
+        int totalHourtime = (differenceTime /60);
+        int totalMintime = (differenceTime % 60);
+        System.out.println(totalHourtime + " hours " + totalMintime + " minutes");
+    }
+//public static void dayOfWeek()
 }
